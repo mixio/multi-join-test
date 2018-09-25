8eae35d078642032ad5265e77c37aa791372cfc8
Jean Jourdain <jean.jourdain@mixio.com>
2018-09-25 10:55:21 +0200

    Added Scripts/edit_db_modules.sh to make DB modules swift package editable.

--- Scripts/edit_db_modules.sh
+++ Scripts/edit_db_modules.sh
@@ -1,13 +1,56 @@
 #!/bin/sh
 
-swift package edit SQL --branch dev
-swift package edit SQLite --branch dev
-swift package edit MySQL --branch dev
-swift package edit PostgreSQL --branch dev
-swift package edit Fluent --branch dev
-swift package edit FluentSQLite --branch dev
-swift package edit FluentMySQL --branch dev
-swift package edit FluentPostgreSQL --branch dev
-swift package edit DatabaseKit --branch dev
+# This script should be inside the <project_directory>/Scripts directory.
 
-swift package generate-xcodeproj
+SCRIPT_NAME="$( basename "$0" )"
+#echo "$SCRIPT_NAME"
+
+SCRIPT_DIR="$( dirname "$0" )"
+#echo "$SCRIPT_DIR"
+
+PROJECT_PATH="$( dirname "$SCRIPT_DIR" )"
+#echo "$PROJECT_PATH"
+
+FORKS_PATH="$( dirname "$PROJECT_PATH" )"
+#echo "$FORKS_PATH"
+
+cd "$PROJECT_PATH"
+
+declare -a modules=(
+    "SQL vapor-sql-mixio-git"
+    "SQLite vapor-sqlite-mixio-git"
+    "MySQL vapor-mysql-mixio-git"
+    "PostgreSQL vapor-postgresql-mixio-git"
+    "Fluent vapor-fluent-mixio-git"
+    "FluentSQLite vapor-fluent-sqlite-mixio-git"
+    "FluentMySQL vapor-fluent-mysql-mixio-git"
+    "FluentPostgreSQL vapor-fluent-postgresql-mixio-git"
+    )
+
+read -p "Make DB modules editable? (y/n): " choice
+case "$choice" in
+  y|Y )
+    for i in "${modules[@]}"
+    do
+        declare -a module=($i)
+        module_name="${module[0]}"
+        module_path="$FORKS_PATH/${module[1]}"
+        echo "Make $module_name editable at $module_path"
+        swift package edit "$module" --path "$module_path"
+    done
+    swift package generate-xcodeproj
+    ;;
+  n|N )
+    for i in "${modules[@]}"
+    do
+        declare -a module=($i)
+        module_name="${module[0]}"
+        echo "Make $module_name uneditable"
+        swift package unedit "$module_name"
+    done
+    swift package generate-xcodeproj
+    ;;
+  * )
+    echo "invalid"
+    ;;
+esac
