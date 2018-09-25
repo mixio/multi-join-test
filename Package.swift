eddd17093e36195128115f25055bcef29b51ec84
Jean Jourdain <jean.jourdain@mixio.com>
2018-09-25 17:49:49 +0200

    New branch mixio-dev-v0.0.1

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
 
         // 🔵 Swift ORM (queries, models, relations, etc) built.
 //      .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        .package(url: "https://github.com/mixio/fluent-sqlite.git", .branch("mixio-dev")),
+        .package(url: "https://github.com/mixio/fluent-sqlite.git", .branch("mixio-dev-v0.0.1")),
 //      .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
-        .package(url: "https://github.com/mixio/fluent-mysql.git", .branch("mixio-dev")),
+        .package(url: "https://github.com/mixio/fluent-mysql.git", .branch("mixio-dev-v0.0.1")),
 //      .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        .package(url: "https://github.com/mixio/fluent-postgresql.git", .branch("mixio-dev")),
+        .package(url: "https://github.com/mixio/fluent-postgresql.git", .branch("mixio-dev-v0.0.1")),
 
         // 🛠 JJ Tools.
         .package(url: "https://github.com/mixio/jjtools.git", from: "0.1.0"),
